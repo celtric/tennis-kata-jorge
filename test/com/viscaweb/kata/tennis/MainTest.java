f73a9e623fd2cce4a3125e783d9c5f8fb0ebb238
Ricard Osorio <celtric@gmail.com>
2016-12-23 09:49:40 +0100

    use primitive booleans to increase readability

--- test/com/viscaweb/kata/tennis/MainTest.java
+++ test/com/viscaweb/kata/tennis/MainTest.java
@@ -8,19 +8,19 @@ import static org.junit.Assert.assertEquals;
 public class MainTest {
 
     @Test public void should_win_player_1_after_score_at_least_4_times_and_2_more_than_player_2() {
-        Result expected = new Result("Player 1 win", Boolean.FALSE, "", Boolean.FALSE);
+        Result expected = new Result("Player 1 win", false, "", false);
 
         assertEquals(expected, win());
     }
 
     @Test public void should_deuce_after_both_players_score_3_times() {
-        Result expected = new Result("", Boolean.FALSE, "", Boolean.TRUE);
+        Result expected = new Result("", false, "", true);
 
         assertEquals(expected, deuce());
     }
 
     @Test public void should_be_advantage_after_player_2_score_3_times_and_player_1_score_4_times() {
-        Result expected = new Result("", Boolean.TRUE, "Player 1", Boolean.FALSE);
+        Result expected = new Result("", true, "Player 1", false);
 
         assertEquals(expected, advantage());
     }
