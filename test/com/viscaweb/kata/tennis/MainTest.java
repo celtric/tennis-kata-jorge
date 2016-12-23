e93facb77fef20b1280562aa0093a50669416532
Ricard Osorio <celtric@gmail.com>
2016-12-23 09:02:41 +0100

    remove Game class that contained test-only methods, and move them to test class

--- test/com/viscaweb/kata/tennis/MainTest.java
+++ test/com/viscaweb/kata/tennis/MainTest.java
@@ -1,33 +1,57 @@
 package com.viscaweb.kata.tennis;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
-    private Game game;
-
-    @Before public void setUp() throws Exception {
-        game = new Game();
-    }
-
     @Test public void should_win_player_1_after_score_at_least_4_times_and_2_more_than_player_2() {
         Result expected = new Result("Player 1 win", Boolean.FALSE, null, Boolean.FALSE);
 
-        assertEquals(expected, game.win());
+        assertEquals(expected, win());
     }
 
     @Test public void should_deuce_after_both_players_score_3_times() {
         Result expected = new Result(null, Boolean.FALSE, null, Boolean.TRUE);
 
-        assertEquals(expected, game.deuce());
+        assertEquals(expected, deuce());
     }
 
     @Test public void should_be_advantage_after_player_2_score_3_times_and_player_1_score_4_times() {
         Result expected = new Result(null, Boolean.TRUE, "Player 1", Boolean.FALSE);
 
-        assertEquals(expected, game.advantage());
+        assertEquals(expected, advantage());
+    }
+
+    //---[ Helpers ]--------------------------------------------------------------------//
+
+    private Result win() {
+        final Score finalScore = new Score(40, 0)
+                .sumScoreToPlayer1();
+        return finalScore.checkWin.apply(null);
+    }
+
+    private Result deuce() {
+        final Score finalScore = new Score(0, 0)
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer2()
+                .sumScoreToPlayer2()
+                .sumScoreToPlayer2();
+        return finalScore.checkDeuce.apply(null);
+    }
+
+    private Result advantage() {
+        final Score finalScore = new Score(0, 0)
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer1()
+                .sumScoreToPlayer2()
+                .sumScoreToPlayer2()
+                .sumScoreToPlayer2()
+                .sumScoreToPlayer1();
+        return finalScore.checkAdvantage.apply(null);
     }
 }
